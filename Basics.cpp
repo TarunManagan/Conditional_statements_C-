7be7fdc1fd5c24f5aef0a9d4317b6c61a91087e3
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 14:24:34 +0530

    Create Basics.cpp

---
+++ Basics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   bool imagecolor = true;
+   if(imagecolor){ //Checks whether imagecolor is true or not, and if true it prints the below statement or else, prints the statement in the else part.
+    cout<<"Image is colorfull";
+   }else{
+    cout<<"Image is not colorfull";
+   }
+return 0;
+}
+/*  OUTPUT
+Image is colorfull
+*/
