32d11ae649c00adbb8627e506250198e802acbe8
TarunManagan <139469256+TarunManagan@users.noreply.github.com>
2023-10-03 14:33:14 +0530

    Update Even_Odd.cpp

--- Even_Odd.cpp
+++ Even_Odd.cpp
@@ -6,7 +6,7 @@ int main() {
    cout<<"Enter the value a: ";
    cin>>a;
 
-   if(a%2==0){ //Checks whether the number is even or odd
+   if(a%2==0){ //Checks whether the number is even or odd as even numbers are divisible by 2 and hence the remainder will be 0
        if(a==0){//Checks whether the number is equal to 0 or not, if true then it prints Zero but if not, then it prints the statement in the else part.
            cout<<"Zero";
        }else{
